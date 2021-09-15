5426bff8829698e315ed638d089381f686cce457
LANGAN84 <langan.kg@gmail.com>
2021-09-15 12:45:29 +0100

    each_with_index_commit

--- directory.rb
+++ directory.rb
@@ -1,17 +1,15 @@
-#let's put all students into an array
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :November},
-{name: "Darth vader", cohort: :November},
-{name: "Nurse Ratched", cohort: :November},
-{name: "Michael Corleone", cohort: :November},
-{name: "Alex DeLarge", cohort: :November},
-{name: "The Wicked Witch of the West", cohort: :November},
-{name: "Terminator", cohort: :November},
-{name:"Freddy Krueger", cohort: :November},
-{name:"The Joker", cohort: :November},
-{name:"Joffrey Baratheon", cohort: :November},
-{name:"Norman Bates", cohort: :November}
-]
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice."
+    students = []
+    name = gets.chomp
+    while !name.empty? do
+        students << {name: name, cohort: :November}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    students
+end
 
 def print_header
   puts "The students of `Villians Academy"
@@ -24,10 +22,25 @@ def print(students)
   end
 end
 
-def print_footer(names)
-   puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+   puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
+
+# students = [
+# {name: "Dr. Hannibal Lecter", cohort: :November},
+# {name: "Darth vader", cohort: :November},
+# {name: "Nurse Ratched", cohort: :November},
+# {name: "Michael Corleone", cohort: :November},
+# {name: "Alex DeLarge", cohort: :November},
+# {name: "The Wicked Witch of the West", cohort: :November},
+# {name: "Terminator", cohort: :November},
+# {name:"Freddy Krueger", cohort: :November},
+# {name:"The Joker", cohort: :November},
+# {name:"Joffrey Baratheon", cohort: :November},
+# {name:"Norman Bates", cohort: :November}
+# ]
