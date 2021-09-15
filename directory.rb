34c781a727e207309bf992cf6d8f6304ecf3f5a0
LANGAN84 <langan.kg@gmail.com>
2021-09-15 15:47:29 +0100

    filters_commit

--- directory.rb
+++ directory.rb
@@ -1,15 +1,17 @@
 def input_students
-    puts "Please enter the names of the students"
+    puts "Please enter the names of the students, their cohort start month, country of birth and age seperated by a comma and a single space"
     puts "To finish, just hit return twice."
     students = []
-    name = gets.chomp
-    while !name.empty? do
-        students << {name: name, cohort: :November}
+    input = gets.chomp.split(", ")
+    while !input.empty? do
+        students << {name: input[0], cohort: input[1], country: input[2], age: input[3]}
         puts "Now we have #{students.count} students"
-        name = gets.chomp
+    input = gets.chomp
     end
    students
 end
+
+#Add more information: hobbies, country of birth and `age`, etc.
 
 def print_header
     puts "The students of `Villians Academy"
@@ -18,26 +20,26 @@ end
 
 def print(students)
     students.each_with_index do |student, index|
-        puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort"     
+        puts "#{index + 1} #{student[:name]}, #{student[:cohort]} cohort, #{student[:country]}, age #{student[:age]}"     
     end
 end
 
-def filter_by_initial_letter(students)
-    puts "Please type in a letter to filter by first name."
-    filter_letter = gets.chomp
-    while !filter_letter.empty? do
-    selected_students = students.select { |student| student[:name].start_with?(filter_letter)}
-        selected_students.each_with_index do |student, index|
-        puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort"     
-        end
-    filter_letter = gets.chomp
-    end
-end
+# def filter_by_initial_letter(students)
+#     puts "Please type in a letter to filter by first name."
+#     filter_letter = gets.chomp
+#     while !filter_letter.empty? do
+#     selected_students = students.select { |student| student[:name].start_with?(filter_letter)}
+#         selected_students.each_with_index do |student, index|
+#         puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort"     
+#         end
+#     filter_letter = gets.chomp
+#     end
+# end
 
-def filter_by_names_under_12_characters(students)
-    selected_students = students.select { |student| student[:name].size <= 12 }
-    print selected_students
-end
+# def filter_by_names_under_12_characters(students)
+#     selected_students = students.select { |student| student[:name].size <= 12 }
+#     print selected_students
+# end
 
 
 def print_footer(students)
@@ -47,8 +49,8 @@ end
 students = input_students
 print_header
 print(students)
-filter_by_initial_letter(students)
-filter_by_names_under_12_characters(students)
+# filter_by_initial_letter(students)
+# filter_by_names_under_12_characters(students)
 print_footer(students)
 
 # students = [
