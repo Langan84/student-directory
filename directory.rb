ac9af55ba2f59548f920721241d1a314bbd072bd
LANGAN84 <langan.kg@gmail.com>
2021-09-16 15:58:47 +0100

    save_student commit

--- directory.rb
+++ directory.rb
@@ -44,6 +44,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -59,6 +60,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit 
     else 
@@ -66,6 +69,15 @@ def process(selection)
     end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 # def filter_by_initial_letter(students)
 #     puts "Please type in a letter to filter by first name."
 #     filter_letter = gets.chomp
